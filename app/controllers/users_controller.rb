532ade65b6abd6f6382ba62982e6093bed069e90
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-09-30 21:09:46 +0000

    users crud finished, all tests passsed except one pertaining to posts

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+  def index
+    matching_users = User.all
+    @list_of_users = matching_users.order({ :created_at => :desc})
+
+    render({ :template => "user_templates/index"})
+  end
+
+  def show
+    the_username = params.fetch("username")
+    @the_user = User.find_by(username: the_username)
+
+    render({ :template => "user_templates/show"})
+  end  
+
+  def create 
+    @the_user = User.new
+    @the_user.username = params.fetch("query_user_name")
+  
+    if @the_user.valid?
+      @the_user.save
+      redirect_to("/users/#{@the_user.username}", { :notice => "User created succesfully"})
+    else
+      redirect_to("/users", { :notice => "User failed to create successfully"})
+    end
+  end
+
+  def update
+    the_username = params.fetch("username")
+    @the_user = User.find_by(username: the_username)
+
+    @the_user.username = params.fetch("query_user_name")
+
+    if @the_user.valid?
+      @the_user.save
+      redirect_to("/users/#{@the_user.username}", { :notice => "User updated successfully"})
+    else
+      redirect_to("/users/#{@the_user.username}", { :notice => "User failed to update successfully"})
+    end
+  end
+
+  def delete
+    the_id = params.fetch("path_id")
+    @the_user = User.find("the_id")
+
+    @the_user.destroy
+
+    redirect_to("/users", { :notice => "User deleted successfully"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "users", :action => "index"})
 
+  get("/users", { :controller => "users", :action => "index"})
+  get("/users/:username", { :controller => "users", :action => "show"})
+  post("/insert_user", { :controller => "users", :action => "create"})
+  post("/modify_user/:username", { :controller => "users", :action => "update"})
+  get("/delete_user/:path_id", { :controller => "users", :action => "destroy"})
 end
